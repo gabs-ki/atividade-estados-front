cc246dd84beb37994085b06a08f3af5c1236723c
marceloGabriel <log4304.016@gmail.com>
2023-03-21 21:40:42 -0300

    Entendendo Exercício

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 'use strict'
 
-import { teste } from "http://localhost:8080/v1/senai/estado/sigla/:uf"
 
 
 const mapa = document.querySelector('svg')
@@ -11,18 +10,13 @@ const mapa = document.querySelector('svg')
 const getEstados = async (event) => {
 
     const estado = event.target.id.replace('BR-', '')
-    const cep = await pesquisarCep(estado)
-
-    const titleEsfera = document.createElement('h1')
-    titleEsfera.classList.add('card__title')
-    titleEsfera.textContent = cep.estado
-
+    
+    console.log(estado)
 }
 
-mapa.addEventListener('click', (event, contato) => {
+mapa.addEventListener('click', (event) => {
 
-    let container = document.getElementById('card__container')
-    container.replaceChildren()
+  
     getEstados(event)
    
 })
